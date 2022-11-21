e022648479388f4104fd5f25b731bb98bf8eeec6
Pattu9626 <pattu50k@gmail.com>
2022-11-21 13:43:09 +0530

    new code added

--- src/org/test/Sample.java
+++ src/org/test/Sample.java
@@ -3,6 +3,11 @@ package org.test;
 public class Sample {
 	private void sysout() {
 		System.out.println("hiii");
+		System.out.println("hello");
+	}
+	private void m1() {
+		System.out.println("practice");
+
 	}
 
 }
